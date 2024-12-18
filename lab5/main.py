d7812e8d0a57ff61cb327ca812c50829bb0b6560
knowlenger <znaevas@mail.ru>
2024-12-18 18:58:45 +0600

    fix: added tests

--- lab5/main.py
+++ lab5/main.py
@@ -2,6 +2,23 @@ from lab5.Classes.Lexer import Lexer
 from lab5.Classes.TableParser import TableParser
 from lab5.Classes.StateLR import StateLR
 
+def check_file(file_path, output_path):
+    with open(file_path, 'r') as file:
+        lexer.get_symbols(file.read())
+    state = StateLR(lexer)
+    try:
+        while state.state_name != 'END':
+            state.next()
+        with open(output_path, 'w') as file:
+            file.write('State action logs: \n')
+            for log in state.logs:
+                file.write(' '.join(log) + '\n')
+    except Exception as e:
+        with open(output_path, 'w') as file:
+            file.write('Wrong sentence')
+
+
+
 
 if __name__ == "__main__":
     table_path = 'my_LL.xlsx'
@@ -9,16 +26,5 @@ if __name__ == "__main__":
     table_parser = TableParser(lexer)
     table_parser.parse_main_table('my_LR.xlsx')
     table_parser.parse_info_table('rule_info.xlsx')
-    with open('input_example.txt', 'r') as file:
-        lexer.get_symbols(file.read())
-    state = StateLR(lexer)
-    try:
-        while state.state_name != 'END':
-            state.next()
-        with open('example_output.txt', 'w') as file:
-            file.write('Логи работы автомата: \n')
-            for log in state.logs:
-                file.write(' '.join(log) + '\n')
-    except Exception as e:
-        with open('example_output.txt', 'w') as file:
-            file.write('Выражение не принадлежит языку')
+    check_file('input_example.txt', 'example_output.txt')
+    check_file('wrong_example.txt', 'example_output_wrong.txt')
